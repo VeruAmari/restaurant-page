841984655f3b1457a99679f9cbedacb5d2c19031
Veru <43966969+VeruAmari@users.noreply.github.com>
2023-11-01 15:12:13 -0300

    Start development mode with source-maps

--- src/home.js
+++ src/home.js
@@ -4,6 +4,5 @@ export function makeHome (parent) {
   const div = newElm("div", "", "home-container", parent);
   newElm("h1", "Welcome to Squirrel Coffee", "header", div);
   newElm("p", "The coziest coffee shop ran by squirrels, to enjoy a squirrel's taste in coffee.", "welcome text", div);
-  newElm()
   return div;
 };
